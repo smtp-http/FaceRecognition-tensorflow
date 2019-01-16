2737586ae905136ca55da29de04027ba53885612
kevinzu <zukeqiang@foxmail.com>
2019-01-16 10:29:57 +0800

    Face collect

--- get_my_faces.py
+++ get_my_faces.py
@@ -129,39 +129,3 @@ for p in processes:
 while 1:
     time.sleep(1)
 
-"""
-index = 2499
-while True:
-    if (index <= 10000):
-        print('Being processed picture %s' % index)
-        # 从摄像头读取照片
-        success, img = camera.read()
-        # 转为灰度图片
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # 使用detector进行人脸检测
-        dets = detector(gray_img, 1)
-
-        for i, d in enumerate(dets):
-            x1 = d.top() if d.top() > 0 else 0
-            y1 = d.bottom() if d.bottom() > 0 else 0
-            x2 = d.left() if d.left() > 0 else 0
-            y2 = d.right() if d.right() > 0 else 0
-
-            face = img[x1:y1,x2:y2]
-            # 调整图片的对比度与亮度， 对比度与亮度值都取随机数，这样能增加样本的多样性
-            face = relight(face, random.uniform(0.5, 1.5), random.randint(-50, 50))
-
-            face = cv2.resize(face, (size,size))
-
-            cv2.imshow('image', face)
-
-            cv2.imwrite(output_dir+'/'+str(index)+'.jpg', face)
-
-            index += 1
-        key = cv2.waitKey(30) & 0xff
-        if key == 27:
-            break
-    else:
-        print('Finished!')
-        break
-"""
